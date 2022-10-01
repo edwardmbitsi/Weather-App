1514055b26f5be19d82a31d007229778157351a8
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-01 18:45:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ displayWeather: function (data) {
       document.querySelector(".weather").classList.remove("loading");  
       document.body.style.backgroundImage="url('https://source.unsplash.com/random/1600x900/?"+ name +")";  
     },  
+search: function () {  
+      this.fetchWeather(document.querySelector(".search-bar").value);  
+    }  
+  };  
