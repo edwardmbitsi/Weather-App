08a6aeeb12a6ce09274ce11a85553c3f99ab9f14
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-01 18:38:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let weather = {  
     "apiKey": "e2c1d873e17655abe9c9bdf6946db59d",  
     fetchWeather: function (city) {  
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid="+ this.apiKey)  
+      .then((response)=>response.json())  
+      .then((data)=>this.displayWeather(data));  
+    },  
