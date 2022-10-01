8169fbe4ab21374846290b79d078c20efc664fed
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-01 18:48:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,3 +27,8 @@ displayWeather: function (data) {
   document.querySelector(".search button").addEventListener("click", function () {  
     weather.search();    
   });  
+ document.querySelector(".search-bar").addEventListener("keyup",function(event) {  
+    if (event.key=="Enter") {  
+      weather.search()  
+    }   
+  });  
